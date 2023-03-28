dc64bdd0728799b3a34719007299d876422aafc1
thien1pro1 <71083034+thien1pro1@users.noreply.github.com>
2023-03-29 01:27:25 +0700

    Update CriteriaRepository.java

--- ChangingE/ChangingE/src/main/java/com/Group/ChangingE/repository/CriteriaRepository.java
+++ ChangingE/ChangingE/src/main/java/com/Group/ChangingE/repository/CriteriaRepository.java
@@ -3,6 +3,7 @@ package com.Group.ChangingE.repository;
 import com.Group.ChangingE.entity.Criteria;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,8 +12,13 @@ import java.util.List;
 public interface CriteriaRepository extends JpaRepository<Criteria, Long> {
 
 //    @Query("SELECT c FROM criteria c WHERE c.parent = ?1")
-    List<Criteria> findCriteriaByParent(Long parent);
-    List<Criteria> searchCriteriaByParentContains(Long id);
-    List<Criteria> searchCriteriaByLevelContains(int level);
+    List<Criteria> findByParent(Long parent);
+//    List<Criteria> searchCriteriaByParentContains(Long id);
+    List<Criteria> findByLevel(int level);
+
+    @Query("SELECT c FROM Criteria c WHERE c.level = :level GROUP BY c.parent ORDER BY c.parent")
+    List<Criteria> findCriteriaByLevelGroupByParentOrderByParent(@Param("level") int level);
+
+    List<Criteria> findCriteriaByLevel(int level);
 
 }
