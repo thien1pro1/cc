b084b40c7c0b4eef22f4afbcf15907bec8bc9f30
Nguyen Dang Thien <dang-thien@system-exe.com.vn>
2023-03-28 12:19:32 +0700

    cc

---
+++ ChangingE/ChangingE/src/test/java/com/Group/ChangingE/ChangingEApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Group.ChangingE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangingEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
